af7e2e06390ebbe5608488d3de12b875844ec765
wangxiao1 <wangxiao1@tuhu.cn>
2020-07-22 10:41:51 +0800

    并发锁注解

---
+++ concurrence-tools/src/main/java/com/fxx/common/tools/concurrence/aspect/NoRepeatSubmit.java
@@ -0,0 +1,19 @@
+package com.fxx.common.tools.concurrence.aspect;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NoRepeatSubmit {
+
+    /**
+     * 设置请求锁定时间
+     */
+    int lockTime() default 3;
+}
---
+++ concurrence-tools/src/main/java/com/fxx/common/tools/concurrence/aspect/NoRepeatSubmitAspect.java
@@ -0,0 +1,69 @@
+package com.fxx.common.tools.concurrence.aspect;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.fxx.common.tools.concurrence.DistributorLockInter;
+import com.fxx.common.tools.exception.ToolAssert;
+import com.fxx.common.tools.exception.ToolException;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author
+ */
+@Aspect
+@Slf4j
+public class NoRepeatSubmitAspect {
+
+    public static final String   AUTHORIZATION = "Authorization";
+
+    private DistributorLockInter distributorLock;
+
+    @Pointcut("@annotation(noRepeatSubmit)")
+    public void pointCut(NoRepeatSubmit noRepeatSubmit) {
+    }
+
+    @Around("pointCut(noRepeatSubmit)")
+    public Object around(ProceedingJoinPoint pjp, NoRepeatSubmit noRepeatSubmit) throws Throwable {
+        int lockSeconds = noRepeatSubmit.lockTime();
+        String url = pjp.getTarget().getClass().toString() + "_" + pjp.getSignature().getName();
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        ToolAssert.notNull(attributes, "NoRepeatSubmitAspect ServletRequestAttributes 为空");
+
+        HttpServletRequest request = attributes.getRequest();
+        ToolAssert.notNull(request, "NoRepeatSubmitAspect HttpServletRequest 为空");
+
+        String token = request.getHeader(AUTHORIZATION);
+        ToolAssert.hasText(token, "NoRepeatSubmitAspect token 为空");
+
+        String key = "NoRepeatSubmitAspect_tryLock_" + url + "_" + token;
+        Object obj = distributorLock.getDistributorLock(key, lockSeconds);
+        log.info("tryLock key = [{}]", key);
+        if (obj != null) {
+            log.info("tryLock success, key = [{}]", key);
+            // 获取锁成功
+            Object result;
+            try {
+                // 执行进程
+                result = pjp.proceed();
+            } finally {
+                //解锁
+                distributorLock.releaseLock(key, obj.toString());
+                log.info("releaseLock success, key = [{}]", key);
+            }
+            return result;
+        } else {
+            // 获取锁失败，认为是重复提交的请求
+            log.info("tryLock fail, key = [{}]", key);
+            throw new ToolException("TOO_MANY_REQUEST!");
+        }
+    }
+}
